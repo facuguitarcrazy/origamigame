43b85bb2e07b0128a2dcbbca0bf8050e477637e1
facuguitarcrazy <facundo_sb@icloud.com>
2015-07-11 10:07:11 +0200

    test

--- Origami Game/AppDelegate.m
+++ Origami Game/AppDelegate.m
@@ -42,9 +42,6 @@
     [FBSDKAppEvents activateApp];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:
-}
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
     return [[FBSDKApplicationDelegate sharedInstance] application:application
